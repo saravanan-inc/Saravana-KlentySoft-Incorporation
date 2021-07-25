f4f640a2fa32770d1a50f0fef36a05db6d65b1e6
saravanan <saravananar001@gmail.com>
2021-07-25 19:15:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var slideIndex = 1
+showSlides(slideIndex)
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    let slides = document.querySelectorAll('.slides');
+    
+    if (n > slides.length) {
+        slideIndex =1
+    }
+    if(n < 1) {
+        slideIndex = slides.length
+    }
+
+    for(i=0; i<slides.length; i++){
+        slides[i].style.display = 'none'
+    }
+    slides[slideIndex-1].style.display = 'block'
+}
